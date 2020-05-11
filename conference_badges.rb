7ab34ccdbeba3766cd2cf731a9a6b7a1a7bf7b41
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-11 23:37:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 name = "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker (name) 
-  name = "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #{name}."
 end
 
